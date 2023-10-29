44b23e618706a59fdf1cf171c337c27140837b0e
Daniel Londono <daniellondono17@gmail.com>
2023-10-29 18:07:53 -0400

    Added all the components for Robin card

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
+/*
+*   Spiderman
+*/
+
 const spidermanButton = document.querySelector('#spider-button1');
 const modal = document.querySelector('.modal');
 const closeIcon = document.querySelector('.modal__content--close');
 
-
-closeIcon.addEventListener('click',closeIconDisplay);
-document.addEventListener('keydown', closeIconKeyEsc);
 spidermanButton.addEventListener('click', modalDisplay);
-
-
-function closeIconDisplay(){
-        modal.classList.add('hidden');
-        modal.classList.remove('visible');
-};
-
 
 function closeIconKeyEsc(event) {
     if (event.key === 'Escape' || event.keyCode === 27) {
@@ -21,9 +15,57 @@ function closeIconKeyEsc(event) {
     }
 }
 
+closeIcon.addEventListener('click',closeIconDisplay);
+document.addEventListener('keydown', closeIconKeyEsc);
+
+function closeIconDisplay(){
+        modal.classList.add('hidden');
+        modal.classList.remove('visible');
+};
+
+
 function modalDisplay(){
     modal.classList.remove('hidden');
     modal.classList.add('visible');
 };
+
+/*
+*   Nightwing
+*/
+
+const nightwingButton = document.querySelector('#nightwing-button1');
+const modalNw = document.querySelector('.modalNw')
+const closeIconNw = document.querySelector('.modalNw__content--close');
+
+
+nightwingButton.addEventListener('click', modalDisplayNw);
+
+function closeIconKeyEscNw(event) {
+    if (event.key === 'Escape' || event.keyCode === 27) {
+        modalNw.classList.add('hidden');
+        modalNw.classList.remove('visible');
+    }
+}
+
+closeIconNw.addEventListener('click',closeIconDisplayNw);
+document.addEventListener('keydown', closeIconKeyEscNw);
+
+function closeIconDisplayNw(){
+    modalNw.classList.add('hidden');
+    modalNw.classList.remove('visible');
+};
+
+
+function modalDisplayNw(){
+    modalNw.classList.remove('hidden');
+    modalNw.classList.add('visible');
+};
+
+
+
+
+
+
+
 
 
