2174ea7185c5475c6358e652f3f48d04dbf662a4
Daniel Londono <daniellondono17@gmail.com>
2023-11-22 18:02:19 -0500

    Latest page update

---
+++ indexLatest.js
@@ -0,0 +1,4 @@
+function changeText() {
+    var displayElement = document.getElementById("displayText");
+    displayElement.textContent = "This text is changed dynamically using JavaScript!";
+  }
