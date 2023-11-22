21c595655ca8e015e6b9831ee4d4af0228f4f211
Daniel Londono <daniellondono17@gmail.com>
2023-11-22 18:53:11 -0500

    Initial version of slider

--- slider.js
+++ slider.js
@@ -1,15 +1,14 @@
-let index = 0;
+let slideIndex = 0;
+const slides = document.querySelector('.slider-images');
 const images = document.querySelectorAll('.slider-images img');
+const totalSlides = images.length;
 
-function slide() {
-  for (let i = 0; i < images.length; i++) {
-    images[i].style.display = 'none';
+function moveSlide(n) {
+  slideIndex += n;
+  if (slideIndex < 0) {
+    slideIndex = totalSlides - 1;
+  } else if (slideIndex >= totalSlides) {
+    slideIndex = 0;
   }
-  index++;
-  if (index > images.length - 1) {
-    index = 0;
-  }
-  images[index].style.display = 'block';
+  slides.style.transform = `translateX(-${slideIndex * 300}px)`; // Adjust image width
 }
-
-setInterval(slide, 2000);
